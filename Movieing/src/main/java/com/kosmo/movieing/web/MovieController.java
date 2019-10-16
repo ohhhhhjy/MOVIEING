67abc47a712787cef4eca51f118f81a47c60ba34
정명지 <mnn01400@gmail.com>
2019-10-16 17:54:57 +0900

    [1016]무비콘트롤러 만들고 다시

--- Movieing/src/main/java/com/kosmo/movieing/web/MovieController.java
+++ Movieing/src/main/java/com/kosmo/movieing/web/MovieController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MovieController {
 
-	@RequestMapping("/Movieing/Movie/AllMovie.mov")
-	public String blogMain() {
-		
-		
-		return "movie/list/AllMovie.tiles";
-	}
-	
+   @RequestMapping("/Movieing/Movie/AllMovie.mov")
+   public String movieMain() {
+      
+      
+      return "movie/list/AllMovie.tiles";
+   }
+   @RequestMapping("/Movieing/Movie/Genre.mov")
+   public String movieGenre() {
+      return "movie/list/Genre.tiles";
+   }
+   
 }
