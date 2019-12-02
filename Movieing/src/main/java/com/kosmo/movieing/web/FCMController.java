859bbf8804811a3a976d6631467be35fee9da9e4
권혜주 <2k3m54@naver.com>
2019-12-02 10:14:27 +0900

    혜주:1202 fcm

--- Movieing/src/main/java/com/kosmo/movieing/web/FCMController.java
+++ Movieing/src/main/java/com/kosmo/movieing/web/FCMController.java
@@ -1,26 +1,59 @@
 package com.kosmo.movieing.web;
 
 
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class FCMController {
 
 
+	@RequestMapping(value="/Movieing/RegisterToken.mov")
+	public void registerToken(HttpServletRequest request) throws UnsupportedEncodingException, SQLException {
+		Connection conn = null;
+	    PreparedStatement pstm = null;
+	    String sql = null;
+	    String token = null;
+	    String requestMethod = null;
 
+	    request.setCharacterEncoding("utf-8");
 
-	@RequestMapping("/Movieing/FCMTest.mov")
-	public String FCMTest(@RequestParam Map map,Model model) {
+	    try{
+	    	Class.forName("oracle.jdbc.OracleDriver");
+	        String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+	        conn = DriverManager.getConnection(url,"SPRING","SPRING");
 
+	        System.out.println("연결성공");
+	    }catch(Exception e){
+	    	System.out.println("연결실패");
+	        e.printStackTrace();
+	    }
+	    token = request.getParameter("token");
+	    System.out.println("token:"+token);
+	    if( token ==null || token.equals("") ){
+	        System.out.println("토큰값이 전달 되지 않았습니다.");
+	    }else{
+	        // 토큰값 전달시 쿼리문 입력할곳임
+	        sql = "INSERT INTO FCM_TOKENS(ID,TOKEN) VALUES(TOKENS_SEQ.NEXTVAL,?)";
+	        pstm = conn.prepareStatement(sql);
+	        pstm.setString(1,token);
+	        pstm.executeUpdate();
+	        System.out.println("토큰값이 입력되었습니다.");
+	    }
 
-		return "FCMTest.tiles";
+	    pstm.close();
+	    conn.close();
 	}
+
 
 
 
