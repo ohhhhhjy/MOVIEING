28ca217648823f02b4ed1e94926efa833c27ec77
ohhhhhjy <ohhhhhjy@gmail.com>
2019-12-03 19:00:00 +0900

    0

--- Movieing/src/main/java/com/kosmo/movieing/android/AndroidController.java
+++ Movieing/src/main/java/com/kosmo/movieing/android/AndroidController.java
@@ -25,156 +25,158 @@ import com.kosmo.movieing.service.UserService;
 @Controller
 public class AndroidController {
 
-	@Resource(name="userService")
-	private UserService userService;
+   @Resource(name="userService")
+   private UserService userService;
 
-	@Resource(name="evalueWishService")
-	private EvalueWishService evalueWishService;
+   @Resource(name="evalueWishService")
+   private EvalueWishService evalueWishService;
 
-	@Resource(name="movieService")
-	private MovieService movieService;
-
-
-
-	//로그인
-	@ResponseBody
-	@RequestMapping(value="/Movieing/Android/Login.mov")
-	public String login(@RequestParam Map map) {
-
-		boolean isLogin = userService.androidIsLogin(map);
-		JSONObject json = new JSONObject();
-		json.put("isLogin", isLogin?"Y":"N");
-
-		return json.toJSONString();
-	}
-
-
-	//별점매길 리스트 뿌려주기
-	@ResponseBody
-	@RequestMapping(value="/Movieing/Android/EvalueList.mov")
-	public String evalueList(@RequestParam Map map) throws UnsupportedEncodingException {
-
-		map.put("id","KIM");//우선 하드코딩
-		//int evalueCount = evalueWishService.getTotalEvalueCount(map);
-
-		List<MovieDto> movieList = movieService.selectListMovie(map);
-
-		int evalueCount = evalueWishService.getTotalEvalueCount(map);
-
-		List<Map> collections = new Vector<Map>();
-		for(MovieDto dto:movieList) {
-			Map record = new HashMap();
-			record.put("movieNo",dto.getMovieNo());
-			record.put("movieTitle",URLEncoder.encode(dto.getMovieTitle(), "UTF-8"));
-			record.put("movieImg", dto.getMovieImg());
-			record.put("movieCountry", URLEncoder.encode(dto.getMovieCountry(), "UTF-8"));
-			record.put("movieYear", dto.getMovieYear());
-			record.put("evalueCount",evalueCount);
-
-			collections.add(record);
-		}
-
-
-		return JSONArray.toJSONString(collections);
-	}//evalueList
-
-
-	//별점 남기기
-	@ResponseBody
-	@RequestMapping(value="/Movieing/Android/EvalueInsert.mov")
-	public void evalueInsert(@RequestParam Map map) {
-
-		map.put("id","KIM");//우선 하드코딩
-
-		if(map.get("grade").equals("0")) return;
-
-		if(evalueWishService.isEvalue(map)) {//이미 남긴 별점이 있다면,
-			System.out.println("업데이트");
-			evalueWishService.update(map);
-		}
-		else {//별점을 새로 입력하는 경우라면,
-			System.out.println("인서트");
-			evalueWishService.insert(map);
-		}
-	}//evalueInsert
-
-	//추천 리스트 뿌려주기
-	@ResponseBody
-	@RequestMapping(value="/Movieing/Android/RecommendList.mov")
-	public String recommendList(@RequestParam Map map) throws UnsupportedEncodingException {
-
-		map.put("id","KIM");//우선 하드코딩
-		//int evalueCount = evalueWishService.getTotalEvalueCount(map);
-
-		List<MovieDto> movieList = movieService.selectListBestRecommendMovie(map);
-
-		List<Map> collections = new Vector<Map>();
-		for(MovieDto dto:movieList) {
-			Map record = new HashMap();
-			record.put("movieNo",dto.getMovieNo());
-			record.put("movieTitle",URLEncoder.encode(dto.getMovieTitle(), "UTF-8"));
-			record.put("movieImg", dto.getMovieImg());
-			record.put("movieCountry", URLEncoder.encode(dto.getMovieCountry(), "UTF-8"));
-			record.put("movieGenre", URLEncoder.encode(dto.getMovieGenre(), "UTF-8"));
-			record.put("movieOrgTitle",dto.getMovieOrgTitle());
-			record.put("movieYear", dto.getMovieYear());
-			record.put("grade",dto.getGrade());
-			collections.add(record);
-		}
-
-
-		return JSONArray.toJSONString(collections);
-	}//evalueList
-
-
-	@ResponseBody
-	@RequestMapping(value="/Movieing/Android/MovieOne.mov")
-	public String movieOne(@RequestParam Map map) throws UnsupportedEncodingException {
-
-		map.put("id","KIM");//우선 하드코딩
-		//int evalueCount = evalueWishService.getTotalEvalueCount(map);
-
-		MovieDto dto = movieService.selectOne(map);
-
-
-		JSONObject json = new JSONObject();
-
-		json.put("movieNo",dto.getMovieNo());
-		json.put("movieTitle",URLEncoder.encode(dto.getMovieTitle(), "UTF-8"));
-		json.put("movieImg", dto.getMovieImg());
-		json.put("movieCountry", URLEncoder.encode(dto.getMovieCountry(), "UTF-8"));
-		json.put("movieGenre", URLEncoder.encode(dto.getMovieGenre(), "UTF-8"));
-		json.put("movieOrgTitle",dto.getMovieOrgTitle());
-		json.put("movieYear", dto.getMovieYear());
-		json.put("grade",dto.getGrade());
-		//가격
-		json.put("naverPrice",dto.getNaverPrice());
-		json.put("wavvePrice",dto.getWavvePrice());
-		json.put("googlePrice",dto.getGooglePrice());
-
-		json.put("movieContent", URLEncoder.encode(dto.getMovieContent(), "UTF-8"));
-		json.put("movieDirector", URLEncoder.encode(dto.getMovieDirector()==null?"감독":dto.getMovieDirector(), "UTF-8"));
-		json.put("movieGrade", URLEncoder.encode(dto.getMovieGrade(), "UTF-8"));//영화관람가
-
-		return json.toJSONString();
-	}//evalueList
+   @Resource(name="movieService")
+   private MovieService movieService;
 
 
 
-	//보고싶어요 추가하기
-	@ResponseBody
-	@RequestMapping(value="/Movieing/Android/WishInsert.mov")
-	public void wishInsert(@RequestParam Map map) {
+   //로그인
+   @ResponseBody
+   @RequestMapping(value="/Movieing/Android/Login.mov")
+   public String login(@RequestParam Map map) {
 
-		map.put("id","KIM");//우선 하드코딩
+      boolean isLogin = userService.androidIsLogin(map);
+      JSONObject json = new JSONObject();
+      json.put("isLogin", isLogin?"Y":"N");
+      System.out.println("안드 테스트");
 
-		List<EvaluationDto> list = evalueWishService.selectWishList(map);
-		for(EvaluationDto dto:list) {
-			if(dto.getMovieNo().equals(map.get("movieNo")))
-				return;
-		}
-		evalueWishService.insertWish(map);
-	}//wishInsert
+      return json.toJSONString();
+   }
+
+
+   //별점매길 리스트 뿌려주기
+   @ResponseBody
+   @RequestMapping(value="/Movieing/Android/EvalueList.mov")
+   public String evalueList(@RequestParam Map map) throws UnsupportedEncodingException {
+
+      map.put("id","ohhhhhjy");//우선 하드코딩
+      //int evalueCount = evalueWishService.getTotalEvalueCount(map);
+
+      List<MovieDto> movieList = movieService.selectListMovie(map);
+
+      int evalueCount = evalueWishService.getTotalEvalueCount(map);
+
+      List<Map> collections = new Vector<Map>();
+      for(MovieDto dto:movieList) {
+         Map record = new HashMap();
+         record.put("movieNo",dto.getMovieNo());
+         record.put("movieTitle",URLEncoder.encode(dto.getMovieTitle(), "UTF-8"));
+         record.put("movieImg", dto.getMovieImg());
+         record.put("movieCountry", URLEncoder.encode(dto.getMovieCountry(), "UTF-8"));
+         record.put("movieYear", dto.getMovieYear());
+         record.put("evalueCount",evalueCount);
+
+         collections.add(record);
+      }
+
+
+      return JSONArray.toJSONString(collections);
+   }//evalueList
+
+
+   //별점 남기기
+   @ResponseBody
+   @RequestMapping(value="/Movieing/Android/EvalueInsert.mov")
+   public void evalueInsert(@RequestParam Map map) {
+
+      map.put("id","ohhhhhjy");//우선 하드코딩
+
+      if(map.get("grade").equals("0")) return;
+
+      if(evalueWishService.isEvalue(map)) {//이미 남긴 별점이 있다면,
+         System.out.println("업데이트");
+         evalueWishService.update(map);
+      }
+      else {//별점을 새로 입력하는 경우라면,
+         System.out.println("인서트");
+         evalueWishService.insert(map);
+      }
+   }//evalueInsert
+
+   //추천 리스트 뿌려주기
+   @ResponseBody
+   @RequestMapping(value="/Movieing/Android/RecommendList.mov")
+   public String recommendList(@RequestParam Map map) throws UnsupportedEncodingException {
+
+      map.put("id","ohhhhhjy");//우선 하드코딩
+      //int evalueCount = evalueWishService.getTotalEvalueCount(map);
+
+      List<MovieDto> movieList = movieService.selectListBestRecommendMovie(map);
+
+      List<Map> collections = new Vector<Map>();
+      String a = "3.45789";
+      for(MovieDto dto:movieList) {
+         Map record = new HashMap();
+         record.put("movieNo",dto.getMovieNo());
+         record.put("movieTitle",URLEncoder.encode(dto.getMovieTitle(), "UTF-8"));
+         record.put("movieImg", dto.getMovieImg());
+         record.put("movieCountry", URLEncoder.encode(dto.getMovieCountry(), "UTF-8"));
+         record.put("movieGenre", URLEncoder.encode(dto.getMovieGenre(), "UTF-8"));
+         record.put("movieOrgTitle",dto.getMovieOrgTitle());
+         record.put("movieYear", dto.getMovieYear());
+         record.put("grade",Math.round(Double.parseDouble(dto.getGrade()==null?a:dto.getGrade())*10)/10.0);
+         collections.add(record);
+      }
+
+
+      return JSONArray.toJSONString(collections);
+   }//evalueList
+
+
+   @ResponseBody
+   @RequestMapping(value="/Movieing/Android/MovieOne.mov")
+   public String movieOne(@RequestParam Map map) throws UnsupportedEncodingException {
+
+      map.put("id","ohhhhhjy");//우선 하드코딩
+      //int evalueCount = evalueWishService.getTotalEvalueCount(map);
+
+      MovieDto dto = movieService.selectOne(map);
+
+
+      JSONObject json = new JSONObject();
+
+      json.put("movieNo",dto.getMovieNo());
+      json.put("movieTitle",URLEncoder.encode(dto.getMovieTitle(), "UTF-8"));
+      json.put("movieImg", dto.getMovieImg());
+      json.put("movieCountry", URLEncoder.encode(dto.getMovieCountry(), "UTF-8"));
+      json.put("movieGenre", URLEncoder.encode(dto.getMovieGenre(), "UTF-8"));
+      json.put("movieOrgTitle",dto.getMovieOrgTitle());
+      json.put("movieYear", dto.getMovieYear());
+      json.put("grade",dto.getGrade());
+      //가격
+      json.put("naverPrice",dto.getNaverPrice());
+      json.put("wavvePrice",dto.getWavvePrice());
+      json.put("googlePrice",dto.getGooglePrice());
+
+      json.put("movieContent", URLEncoder.encode(dto.getMovieContent(), "UTF-8"));
+      json.put("movieDirector", URLEncoder.encode(dto.getMovieDirector()==null?"감독":dto.getMovieDirector(), "UTF-8"));
+      json.put("movieGrade", URLEncoder.encode(dto.getMovieGrade(), "UTF-8"));//영화관람가
+
+      return json.toJSONString();
+   }//evalueList
+
+
+
+   //보고싶어요 추가하기
+   @ResponseBody
+   @RequestMapping(value="/Movieing/Android/WishInsert.mov")
+   public void wishInsert(@RequestParam Map map) {
+
+      map.put("id","ohhhhhjy");//우선 하드코딩
+
+      List<EvaluationDto> list = evalueWishService.selectWishList(map);
+      for(EvaluationDto dto:list) {
+         if(dto.getMovieNo().equals(map.get("movieNo")))
+            return;
+      }
+      evalueWishService.insertWish(map);
+   }//wishInsert
 
 
 }
--- Movieing/src/main/webapp/WEB-INF/views/admin/admin_announce.jsp
+++ Movieing/src/main/webapp/WEB-INF/views/admin/admin_announce.jsp
@@ -1,11 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+   pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
 
 <div id="page-wrapper">
     <div class="row">
-    	<div class="col-md-12">
+       <div class="col-md-12">
          <div class="page-header">
              <h1>공지사항 관리 페이지</h1>
          </div>
@@ -18,7 +18,7 @@
     </div>
     <!-- /.row -->
     <script id="detailTemplate1" type="text/x-shield-template">
-	<div style="margin:30px;margin-left:80px">
+   <div style="margin:30px;margin-left:80px">
         <table> 
             <tr>             
                <td>
@@ -27,14 +27,14 @@
                </td>
             </tr>
         </table>
-	</div>
-	</script>
-	
+   </div>
+   </script>
+   
     <script>
     var notiData = $("p").html();
     
     $(function(){
-    	$("#grid-announce").shieldGrid({
+       $("#grid-announce").shieldGrid({
             dataSource: {
                 data: notiData
             },
@@ -55,50 +55,49 @@
                     title: "수정",
                     width: "80px",
                     buttons: [
-		            {
-		                cls: "annoedit",
-		                caption: "<div class='annoedit'></div>수정",
-		                commandName: "annoedit",
-		                click: function (rowIndex) {
-		                	
-		                	var no;
-		                	if(rowIndex%2==0)
-		                		no = $('.sui-row:eq('+rowIndex/2+')>.sui-cell:eq(1)').text();
-		                	else
-		                		no = $('.sui-alt-row:eq('+(rowIndex-1)/2+')>.sui-cell:eq(1)').text();
-		                	location.assign("<c:url value='/Movieing/admin/admin_aedit.mov?no="+no+"'/>");
-		                	
-		                }
-		            }]
+                  {
+                      cls: "annoedit",
+                      caption: "<div class='annoedit'></div>수정",
+                      commandName: "annoedit",
+                      click: function (rowIndex) {
+                         
+                         var no;
+                         if(rowIndex%2==0)
+                            no = $('.sui-row:eq('+rowIndex/2+')>.sui-cell:eq(1)').text();
+                         else
+                            no = $('.sui-alt-row:eq('+(rowIndex-1)/2+')>.sui-cell:eq(1)').text();
+                         location.assign("<c:url value='/Movieing/admin/admin_aedit.mov?no="+no+"'/>");
+                         
+                      }
+                  }]
                 },
                 {
                     title: "삭제",
                     width: "80px",
                     buttons: [
-		            {
-		                cls: "annodel",
-		                caption: "<div class='annodel'></div>삭제",
-		                commandName: "annodel",
-		                click: function (rowIndex) {
-		                	
-		                	if(confirm('해당 게시글을 삭제하시겠습니까?')){
-			                	var no;
-			                	if(rowIndex%2==0)
-			                		no = $('.sui-row:eq('+rowIndex/2+')>.sui-cell:eq(1)').text();
-			                	else
-			                		no = $('.sui-alt-row:eq('+(rowIndex-1)/2+')>.sui-cell:eq(1)').text();
-			                	location.assign("<c:url value='/Movieing/admin/admin_adel.mov?no="+no+"'/>");
-		                	}
-		                }
-		            }]
+                  {
+                      cls: "annodel",
+                      caption: "<div class='annodel'></div>삭제",
+                      commandName: "annodel",
+                      click: function (rowIndex) {
+                         
+                         if(confirm('해당 게시글을 삭제하시겠습니까?')){
+                            var no;
+                            if(rowIndex%2==0)
+                               no = $('.sui-row:eq('+rowIndex/2+')>.sui-cell:eq(1)').text();
+                            else
+                               no = $('.sui-alt-row:eq('+(rowIndex-1)/2+')>.sui-cell:eq(1)').text();
+                            location.assign("<c:url value='/Movieing/admin/admin_adel.mov?no="+no+"'/>");
+                         }
+                      }
+                  }]
                 }
             ]
         });
-    	$('#write').click(function(){
-    		location.assign("<c:url value='/Movieing/admin/admin_awrite.mov'/>");
-    	});
+       $('#write').click(function(){
+          location.assign("<c:url value='/Movieing/admin/admin_awrite.mov'/>");
+       });
     });
-    	
+       
     </script>
 </div>
-
